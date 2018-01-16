725c1399c4ed42e46eca51b9a62259f5fbf72d4c
James Chen <jchen3652@gmail.com>
2018-01-15 19:24:44 -0500

    got rid of hard code

--- PIDGraphingUtility/src/charts/PIDChart.java
+++ PIDGraphingUtility/src/charts/PIDChart.java
@@ -1,6 +1,7 @@
 package charts;
-import com.sun.istack.internal.logging.Logger;
-//import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
@@ -9,13 +10,11 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTable;
-
+//import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class PIDChart extends Application {		
 	int i;
-	final int pointsToGraph = 1000;
+	final int pointsToGraph = 100;
 	
 	@Override 
 	public void start(Stage stage) {
@@ -27,10 +26,10 @@ public class PIDChart extends Application {
 		//Sets up network tables connection. This stuff should be commented out when testing graph
 		
 		//NEW NETWORK TABLES CODE
-		/*
+		
 		NetworkTableInstance.getDefault().startClient("roboRIO-500-FRC.local");//localhost //roboRIO-500-FRC.local //roboRIO-502-FRC.local		
 		NetworkTable table = NetworkTableInstance.getDefault().getTable("PIDTuner");
-		*/	
+			
 		
 		/*
 		//OLD NETWORK TABLES CODE
@@ -39,22 +38,20 @@ public class PIDChart extends Application {
 		NetworkTable table = NetworkTable.getTable("PIDTuner");
 		*/
 		
-		
 		try {//Network tables access is slow you must delay 3 seconds to give it a chance to access your requested table 
 			Thread.sleep(3000);
 		} catch (InterruptedException ex) {
 		//do nothing for right now 
 		}
 		
-		
 		//These are the actual array definitions from network tables
 		
-		/*
+		
 		//NEW NETWORK TABLES CODE
-		Double[] ptTimestamp = (Double[]) table.getEntry("PTTimestamp").getNumberArray(clearArray);
+		Double[] ptTimestamp =  (Double[]) table.getEntry("PTTimestamp").getNumberArray(clearArray);
 		Double[] ptMotor = (Double[]) table.getEntry("PTMotor").getNumberArray(clearArray);
 		Double[] ptEncoder = (Double[]) table.getEntry("PTEncoder").getNumberArray(clearArray);
-		*/
+		
 
 		/*
 		//OLD NETWORK TABLES CODE
@@ -62,11 +59,9 @@ public class PIDChart extends Application {
 		Double[] ptMotor = table.getNumberArray("PTMotor", clearArray);  
 		Double[] ptEncoder = table.getNumberArray("PTEncoder", clearArray);  
 		*/
-		
-		
-		//These are the dummy array definitions to test the graph WHEN THERE ARE NO NETWORK TABLES
-		
-			
+				
+		//These are the dummy array d efinitions to test the graph WHEN THERE ARE NO NETWORK TABLES		
+		/*	
 		Double[] ptTimestamp = new Double[pointsToGraph];	
 		Double[] ptMotor = new Double[pointsToGraph];
 		Double[] ptEncoder = new Double[pointsToGraph];
@@ -75,7 +70,7 @@ public class PIDChart extends Application {
 			ptMotor[i] = (double) i;
 			ptEncoder[i] = Math.pow(i, 2);
 		}
-		
+		*/
 		//Set properties for graph window
 		stage.setTitle("FF503 PID Tuning | Written by James Chen and Areeb Rahim");
 		final CategoryAxis xAxis = new CategoryAxis();
@@ -91,8 +86,7 @@ public class PIDChart extends Application {
 		
 		XYChart.Series series2 = new XYChart.Series();
 		series2.setName("Encoder");
-		  		  
-		  
+		  		  		  
 		for(i=0;i<pointsToGraph;i++){ //Graphs the points
 			String s=Double.toString(ptTimestamp[i]);
 			series1.getData().add(new XYChart.Data(s, ptMotor[i]));
@@ -106,9 +100,7 @@ public class PIDChart extends Application {
 		stage.show();
 	}
 
-
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 }
