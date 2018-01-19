48f4b42e0f8c2355d50dc3a7b297bcdb7bd87728
James Chen <jchen3652@gmail.com>
2018-01-18 20:19:18 -0500

    Added Error

--- PIDGraphingUtility/src/charts/PIDChart.java
+++ PIDGraphingUtility/src/charts/PIDChart.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
 
 public class PIDChart extends Application {		
 	int i;
-	final int pointsToGraph = 100;
+	int pointsToGraph;
 	
 	@Override 
 	public void start(Stage stage) {
@@ -22,19 +22,10 @@ public class PIDChart extends Application {
 			clearArray[i]=0.0;
 		}
 		
-		//Sets up network tables connection. This stuff should be commented out when testing graph
-		
-		//NEW NETWORK TABLES CODE
-		
 		NetworkTableInstance.getDefault().startClient("roboRIO-500-FRC.local");//localhost //roboRIO-500-FRC.local //roboRIO-502-FRC.local		
 		NetworkTable table = NetworkTableInstance.getDefault().getTable("PIDTuner");
 		
-		/*
-		NetworkTable.setTeam(500);
-		//OLD NETWORK TABLES CODE
-		NetworkTable.setIPAddress("roboRIO-500-FRC.local");//localhost //roboRIO-500-FRC.local //roboRIO-502-FRC.local
-		NetworkTable table = NetworkTable.getTable("PIDTuner");
-		*/
+
 		
 		try {//Network tables access is slow you must delay 3 seconds to give it a chance to access your requested table 
 			Thread.sleep(3000);
@@ -43,33 +34,16 @@ public class PIDChart extends Application {
 		}
 		
 		//These are the actual array definitions from network tables
-		
-		
-		//NEW NETWORK TABLES CODE
-		
+
+		Double[] ptError = table.getEntry("PTError").getDoubleArray(clearArray);
 		Double[] ptTimestamp = table.getEntry("PTTimestamp").getDoubleArray(clearArray);
 		Double[] ptMotor = table.getEntry("PTMotor").getDoubleArray(clearArray);
 		Double[] ptEncoder =table.getEntry("PTEncoder").getDoubleArray(clearArray);
+		Number numberOfPoints = table.getEntry("NumberOfPoints").getNumber(0);
 		
-
-	/*
-		//OLD NETWORK TABLES CODE
-		Double[] ptTimestamp = table.getNumberArray("PTTimestamp", clearArray);		
-		Double[] ptMotor = table.getNumberArray("PTMotor", clearArray);  
-		Double[] ptEncoder = table.getNumberArray("PTEncoder", clearArray);  
-		*/
-				
-		//These are the dummy array d efinitions to test the graph WHEN THERE ARE NO NETWORK TABLES		
-		/*	
-		Double[] ptTimestamp = new Double[pointsToGraph];	
-		Double[] ptMotor = new Double[pointsToGraph];
-		Double[] ptEncoder = new Double[pointsToGraph];
-		for(int i = 0; i<pointsToGraph; i+=1) {
-			ptTimestamp[i] = (double) i;
-			ptMotor[i] = (double) i;
-			ptEncoder[i] = Math.pow(i, 2);
-		}
-		*/
+		
+		pointsToGraph = numberOfPoints.intValue();
+		
 		//Set properties for graph window
 		stage.setTitle("FF503 PID Tuning | Written by James Chen and Areeb Rahim");
 		final CategoryAxis xAxis = new CategoryAxis();
@@ -84,16 +58,20 @@ public class PIDChart extends Application {
 		series1.setName("Motor");		  
 		
 		XYChart.Series series2 = new XYChart.Series();
-		series2.setName("Encoder");
-		  		  		  
+		series2.setName("Angle");
+		
+		XYChart.Series error = new XYChart.Series();
+		error.setName("Error");
+		
 		for(i=0;i<pointsToGraph;i++){ //Graphs the points
 			String s=Double.toString(ptTimestamp[i]);
 			series1.getData().add(new XYChart.Data(s, ptMotor[i]));
 			series2.getData().add(new XYChart.Data(s, ptEncoder[i]));
+			error.getData().add(new XYChart.Data(s, ptError[i]));
 		}
 		  
 		Scene scene  = new Scene(lineChart,800,600);	   
-		lineChart.getData().addAll(series1, series2);
+		lineChart.getData().addAll(series1, series2, error);
 		 
 		stage.setScene(scene);
 		stage.show();
