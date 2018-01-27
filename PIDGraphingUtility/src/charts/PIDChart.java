88dccde1871ebedecef36d25a682f689cd95be3a
James Chen <jchen3652@gmail.com>
2018-01-27 15:10:34 -0500

    very close to actually working

--- PIDGraphingUtility/src/charts/PIDChart.java
+++ PIDGraphingUtility/src/charts/PIDChart.java
@@ -1,7 +1,6 @@
 package charts;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -13,25 +12,49 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-@SuppressWarnings({ "unchecked", "rawtypes" })
 public class PIDChart extends Application {
-	int i;
-	
-	ArrayList<XYChart.Series> seriesContainer = new ArrayList<XYChart.Series>();
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void start(Stage stage) {
 		Double[] clearArray = new Double[0];
-
-		NetworkTableInstance.getDefault().startClient("roboRIO-500-FRC.local");// localhost //roboRIO-500-FRC.local
-																				// //roboRIO-502-FRC.local
+		NetworkTableInstance.getDefault().startClient("roboRIO-500-FRC.local");// localhost; roboRIO-500-FRC.local;
+																				// roboRIO-502-FRC.local
 		NetworkTable table = NetworkTableInstance.getDefault().getTable("PIDTuner");
 
 		try {// Network tables access is slow you must delay 3 seconds to give it a chance to
-				// access your requested table
 			Thread.sleep(3000);
 		} catch (InterruptedException ex) {
 			// do nothing for right now
+		}
+
+		int numberOfRuns = table.getEntry("PTNumberOfRuns").getNumber(0).intValue();
+
+		ArrayList<Double[]> allTimestamps = new ArrayList<Double[]>();
+		ArrayList<Double[]> allMotors = new ArrayList<Double[]>();
+		ArrayList<Double[]> allEncoders = new ArrayList<Double[]>();
+		ArrayList<Integer> allNumberOfPoints = new ArrayList<Integer>();
+
+		Number ptSetpoint = table.getEntry("Setpoint").getNumber(0);
+
+		for (int i = 0; i < numberOfRuns; i += 1) {
+			Double[] PTTimestamp = table.getEntry("PTTimestamp " + Integer.toString(i + 1)).getDoubleArray(clearArray);
+			Double[] PTMotor = table.getEntry("PTMotor " + Integer.toString(i + 1)).getDoubleArray(clearArray);
+			Double[] PTEncoder = table.getEntry("PTEncoder " + Integer.toString(i + 1)).getDoubleArray(clearArray);
+			Integer PTNumberOfPoints = table.getEntry("NumberOfPoints " + Integer.toString(i + 1)).getNumber(0).intValue();
+			
+			System.out.println("ptnumberofpoints" + PTNumberOfPoints);
+			Number[] setPointArray = new Number[PTNumberOfPoints];
+
+			for (int n = 0; n < PTNumberOfPoints; n += 1) {
+				setPointArray[n] = ptSetpoint;
+
+			}
+
+			allTimestamps.add(PTTimestamp); // 0
+			allMotors.add(PTMotor);// 1
+			allEncoders.add(PTEncoder); // 2
+			allNumberOfPoints.add(PTNumberOfPoints); // 3 //problem happening here
 		}
 
 		// Set properties for graph window
@@ -43,66 +66,40 @@ public class PIDChart extends Application {
 
 		lineChart.setTitle("FF503 PID Tuning");
 		lineChart.setCreateSymbols(false);
-		
-		int numberOfRuns = table.getEntry("PTNumberOfRuns").getNumber(0).intValue();
-		
-		for (int i = 0; i <= numberOfRuns; i+=1) {
-			int n = i + 1;
-			
-			Double[] ptTimestamp = null;
-			Double[] ptMotor = null;
-			Double[] ptEncoder = null;
-			
-			
-			ptTimestamp = table.getEntry("PTTimestamp " + Integer.toString(i+1)).getDoubleArray(clearArray); //creates the arrays, which change every time
-			ptMotor = table.getEntry("PTMotor " + Integer.toString(i+1)).getDoubleArray(clearArray);
-			ptEncoder = table.getEntry("PTEncoder " + Integer.toString(i+1)).getDoubleArray(clearArray);
 
-			int pointsToGraph = table.getEntry("NumberOfPoints " + Integer.toString(n)).getNumber(0).intValue();
+		ArrayList<XYChart.Series> seriesObjectsArray = new ArrayList<XYChart.Series>();
 
-			Number ptSetpoint = table.getEntry("Setpoint " + Integer.toString(n)).getNumber(0);
-			Number[] setPointArray = new Number[pointsToGraph];
-			
-			
-			
+		for (int i = 0; i < numberOfRuns; i++) {
 			XYChart.Series output = new XYChart.Series();
-			output.setName("Motor Output " + Integer.toString(n));
+			output.setName("Motor Output" + Integer.toString(i + 1));
 
 			XYChart.Series angle = new XYChart.Series();
-			angle.setName("Angle " + Integer.toString(n));
+			angle.setName("Angle" + Integer.toString(i + 1));
 
 			XYChart.Series setpoint = new XYChart.Series();
-			setpoint.setName("Setpoint " + Integer.toString(n));
+			setpoint.setName("Setpoint" + Integer.toString(i + 1));
 
-			
-			
-			for (int w = 0; w < pointsToGraph; w += 1) {
-				setPointArray[w] = ptSetpoint;
-			}
-			// temporary code
-			for (int x = 0; x < ptTimestamp.length; x++) { // Graphs the points
-				String s = Double.toString(ptTimestamp[i]);
-				output.getData().add(new XYChart.Data(s, ptMotor[i]));
-				angle.getData().add(new XYChart.Data(s, ptEncoder[i]));
-				setpoint.getData().add(new XYChart.Data(s, setPointArray[i]));
-			}
-
-			seriesContainer.add(output);
-			seriesContainer.add(angle);
-			seriesContainer.add(setpoint);
-
-		}
-			
-		System.out.println(seriesContainer);
-		for (int i = 0; i < numberOfRuns; i++) {
-			lineChart.getData().add(seriesContainer.get(i));
-			lineChart.getData().add(seriesContainer.get(i + 1));
-			lineChart.getData().add(seriesContainer.get(i + 2));
-
-		}
+			//there was a problem on this line
+			System.out.println(allNumberOfPoints.get(i).intValue());
+			for (int n = 0; n < allNumberOfPoints.get(i).intValue(); n+=1) { // Graphs the points
+				
 		
-		Scene scene = new Scene(lineChart, 800, 600);
+				String s = Double.toString(allTimestamps.get(i)[n]);
+				output.getData().add(new XYChart.Data(s, allMotors.get(i)[n]));
+				angle.getData().add(new XYChart.Data(s, allEncoders.get(i)[n]));
+				setpoint.getData().add(new XYChart.Data(s, allEncoders.get(i)[n]));
+			}
+			seriesObjectsArray.add(output);
+			seriesObjectsArray.add(angle);
+			seriesObjectsArray.add(setpoint);
+		}
 
+		Scene scene = new Scene(lineChart, 800, 600);
+		for (int i = 0; i < numberOfRuns; i++) {
+			lineChart.getData().add(seriesObjectsArray.get(i));
+			lineChart.getData().add(seriesObjectsArray.get(i + 1));
+			lineChart.getData().add(seriesObjectsArray.get(i + 2));
+		}
 		stage.setScene(scene);
 		stage.show();
 	}
